fcdb3a2419f6bc8e5a2cca87791718a50bcbfeca
Francis Trujillo <ftrujillo@micron.com>
2016-01-27 13:34:57 -0700

    need validate now

--- src/main/java/org/nve/cliapp/utils/SHAUtils.java
+++ src/main/java/org/nve/cliapp/utils/SHAUtils.java
@@ -5,6 +5,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Random;
 import java.util.logging.Level;
@@ -36,8 +37,9 @@ import javax.crypto.spec.PBEKeySpec;
  */
 public class SHAUtils {
 
-    private static final int ITERATIONS = 1520;
-    private static final int KEY_LENGTH = 512; // bits
+    private static final int ITERATIONS = 1520; // for PBKDF2
+    private static final int KEY_LENGTH = 512; // bits for PBKDF2
+    private static final String PEPPER = "r5zob55OCerlFwGXU3F4aSlIVYQef349KqMmGOjyMQ92aUIrBhecw7anBHwJypHmPa9mKR3q3A+OiT34mrfZeg=="; // for PBKDF2
 
     public SHAUtils() {
     }
@@ -129,7 +131,7 @@ public class SHAUtils {
 //    }
     public static String generateSHA256Hash(String myString) {
         StringBuilder sb = new StringBuilder();
-        
+
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.reset();
@@ -137,47 +139,43 @@ public class SHAUtils {
             byte[] saltByteArray = SHAUtils.base64StringToByteArray(salt);
             md.update(saltByteArray);
             byte byteData[] = md.digest(myString.getBytes());
-//            //convert the byte to hex format method 2
-//            for (int i = 0; i < byteData.length; i++) {
-//                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-//            }
             sb.append(salt).append(":").append(SHAUtils.byteArrayToBase64String(byteData));
 
         } catch (NoSuchAlgorithmException ex) {
             Logger.getLogger(SHAUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
 
         return (sb.toString());
     }
 
-    // http://howtodoinjava.com/optimization/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
     /**
      * Use this one for hashing passwords.
-     * 
+     *
+     * http://howtodoinjava.com/optimization/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+     *
      * @param password
      * @return
      * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException 
+     * @throws InvalidKeySpecException
      */
     public static String generatePBKDF2Hash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
         char[] chars = password.toCharArray();
         String salt = SHAUtils.generateSalt();
         byte[] saltByteArray = SHAUtils.base64StringToByteArray(salt);
 
-        PBEKeySpec spec = new PBEKeySpec(chars, saltByteArray, ITERATIONS, KEY_LENGTH);
+        PBEKeySpec spec = new PBEKeySpec(chars, SHAUtils.pepperedSalt(saltByteArray), ITERATIONS, KEY_LENGTH);
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
         byte[] hash = skf.generateSecret(spec).getEncoded();
-        
+
         return salt + ":" + SHAUtils.byteArrayToBase64String(hash);
     }
 
     public static boolean validatePBKDF2(String originalPassword, String pbkdf2Hash) throws NoSuchAlgorithmException, InvalidKeySpecException {
         String[] parts = pbkdf2Hash.split(":");
-        byte[] salt = SHAUtils.base64StringToByteArray(parts[0]);
+        byte[] saltByteArray = SHAUtils.base64StringToByteArray(parts[0]);
         byte[] hash = SHAUtils.base64StringToByteArray(parts[1]);
-        
-        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, ITERATIONS, KEY_LENGTH);
+
+        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), SHAUtils.pepperedSalt(saltByteArray), ITERATIONS, KEY_LENGTH);
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
         byte[] testHash = skf.generateSecret(spec).getEncoded();
 
@@ -188,44 +186,100 @@ public class SHAUtils {
         return diff == 0;
     }
 
-    
+    /**
+     * Generate a random byte array and then Base64 encode into a String.
+     *
+     * @return Base64 encoded string
+     */
     public static String generateSalt() {
         Random random = new SecureRandom();
-        byte[] bytes = new byte[64];
+        byte[] bytes = new byte[64];  // If you change this, then static PEPPER must change to match length.
         random.nextBytes(bytes);
         return SHAUtils.byteArrayToBase64String(bytes);
     }
 
-    private static String byteArrayToBase64String(byte[] byteArray) {
+    /**
+     * byte[] to Base64 encoded String.
+     *
+     * @param byteArray
+     * @return Base64 encoded String.
+     */
+    public static String byteArrayToBase64String(byte[] byteArray) {
         Base64.Encoder encoder = Base64.getEncoder(); // Java 1.8 ships with this now.
         String base64EncodedString = encoder.encodeToString(byteArray);
         return (base64EncodedString);
     }
 
-    private static byte[] base64StringToByteArray(String myStr) {
+    /**
+     * Base64 encoded to byte[].
+     *
+     * @param myStr
+     * @return byte[]
+     */
+    public static byte[] base64StringToByteArray(String myStr) {
         Base64.Decoder decoder = Base64.getDecoder(); // Java 1.8 ships with this now.
         byte[] byteArray = decoder.decode(myStr);
         return (byteArray);
     }
-    
-//    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
-//        BigInteger bi = new BigInteger(1, array);
-//        String hex = bi.toString(16);
-//        int paddingLength = (array.length * 2) - hex.length();
-//        if (paddingLength > 0) {
-//            return String.format("%0" + paddingLength + "d", 0) + hex;
-//        } else {
-//            return hex;
-//        }
-//    }
-//
-//    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException {
-//        byte[] bytes = new byte[hex.length() / 2];
-//        for (int i = 0; i < bytes.length; i++) {
-//            bytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
-//        }
-//        return bytes;
-//    }
 
+    /**
+     * Not used in SHAUtils, but thought this may come in handy later.
+     *
+     * @param array
+     * @return String padded hex.
+     * @throws NoSuchAlgorithmException
+     */
+    public static String byteArraytoHex(byte[] array) throws NoSuchAlgorithmException {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if (paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        } else {
+            return hex;
+        }
+    }
+
+    /**
+     * Not used in SHAUtils, but thought this may come in handy later.
+     *
+     * @param hex
+     * @return byte[]
+     * @throws NoSuchAlgorithmException
+     */
+    public static byte[] hexToByteArray(String hex) throws NoSuchAlgorithmException {
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+
+    /**
+     * This method will take an input byte[] (salt) and then pepper it up; return new byte[]
+     * 
+     * @param saltByteArray
+     * @return  byte[]
+     */
+    private static byte[] pepperedSalt(byte[] saltByteArray) {
+        byte[] resultByteArray = Arrays.copyOf(saltByteArray, saltByteArray.length);
+        byte[] pepperByteArray = SHAUtils.base64StringToByteArray(PEPPER);
+        boolean debug = false;
+
+        // bitwise XOR
+        for (int ii = 0; ii < saltByteArray.length; ii++) {
+            resultByteArray[ii] ^= pepperByteArray[ii];
+        }
+
+        if (debug) {
+            SysUtils.displayHexDump(saltByteArray);
+            SysUtils.displayHexDump(pepperByteArray);
+            SysUtils.displayHexDump(resultByteArray);
+            String hdr = SysUtils.repeatString("=", 73);
+            System.out.println(hdr);
+        }
+
+        return resultByteArray;
+    }
 
 }
